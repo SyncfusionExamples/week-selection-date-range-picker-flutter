73e0bd25fae2daaad4c558699de5dbd3d62325d0
Nijamudeen Mohamed Sulaiman <nijamudeensulaiman@syncfusion.com>
2020-04-14 16:03:27 +0530

    Sample added

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(WeekSelection());
+
+class WeekSelection extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false ,
+      home: WeekSelectionInPicker(),
+    );
+  }
+}
+
+class WeekSelectionInPicker extends StatefulWidget {
+  @override
+  _WeekSelectionInPickerState createState() => _WeekSelectionInPickerState();
+}
+
+class _WeekSelectionInPickerState extends State<WeekSelectionInPicker> {
+  DateRangePickerController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _controller = DateRangePickerController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: const EdgeInsets.fromLTRB(50, 150, 50, 100),
+            child: SfDateRangePicker(
+              controller: _controller,
+              view: DateRangePickerView.month,
+              selectionMode: DateRangePickerSelectionMode.range,
+              onSelectionChanged: selectionChanged,
+              monthViewSettings: DateRangePickerMonthViewSettings(enableSwipeSelection: false),
+            ),
+          )
+        ],
+      ),
+
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+
+  void selectionChanged(DateRangePickerSelectionChangedArgs args) {
+    int firstDayOfWeek = DateTime.sunday % 7;
+    int endDayOfWeek = (firstDayOfWeek - 1) % 7;
+    endDayOfWeek = endDayOfWeek <  0? 7 + endDayOfWeek : endDayOfWeek;
+    PickerDateRange ranges = args.value;
+    DateTime date1 = ranges.startDate;
+    DateTime date2 = ranges.endDate?? ranges.startDate;
+    if(date1.isAfter(date2))
+      {
+        var date=date1;
+        date1=date2;
+        date2=date;
+      }
+    int day1 = date1.weekday % 7;
+    int day2 = date2.weekday % 7;
+
+    DateTime dat1 = date1.add(Duration(days: (firstDayOfWeek - day1)));
+    DateTime dat2 = date2.add(Duration(days: (endDayOfWeek - day2)));
+
+    if( !isSameDate(dat1, ranges.startDate)|| !isSameDate(dat2,ranges.endDate))
+    {
+      _controller.selectedRange = PickerDateRange(dat1, dat2);
+    }
+  }
+
+  bool isSameDate(DateTime date1, DateTime date2) {
+    if (date2 == date1) {
+      return true;
+    }
+    if (date1 == null || date2 == null) {
+      return false;
+    }
+    return date1.month == date2.month &&
+        date1.year == date2.year &&
+        date1.day == date2.day;
+  }
+}
